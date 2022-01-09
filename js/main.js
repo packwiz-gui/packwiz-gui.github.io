74980a708bf0c86de9147e9f00c28fe81081c0d6
ExoPlant <49562894+ExoPlant@users.noreply.github.com>
2022-01-09 15:41:01 +0000

    Add title

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
 document.getElementsByTagName("h1").item(0).style.display = "none"
 document.getElementById("downloads").style.display = "none"
+const title = document.createElement("title")
+title.innerHTML = "packwiz-gui Docs"
